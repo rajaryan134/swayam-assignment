f795df0ec43d3a1123766aa3bd5f94e271bac2ec
Raj Aryan <rajaryan@rajs-Laptop.local>
2025-11-06 22:10:05 +0530

    Update c.cpp

--- c.cpp
+++ c.cpp
@@ -3,24 +3,24 @@
 #include <cmath>
 using namespace std;
 
-pair<int, int> findmissingnumbers(vector<int>& arr, int n) { //function declare....
+pair<int, int> findmissingnumbers(vector<int>& arr, int n) {
     int totalsum = (n + 2) * (n + 3) / 2;
-    int totalsqsum = (n + 2) * (n + 3) * (2 * (n + 2) + 1) / 6;//total sum and sq of nt no...
+    int totalsqsum = (n + 2) * (n + 3) * (2 * (n + 2) + 1) / 6;
 
     int arrsum = 0, arrsqsum = 0;
     for (int num : arr) {
         arrsum += num;
-        arrsqsum += num * num; //actual sum and sum of sq...
+        arrsqsum += num * num;
     }
 
-    int sumdiff = totalsum - arrsum;      
-    int sqsumdiff = totalsqsum - arrsqsum; //diffference ...
+    int sumdiff = totalsum - arrsum;       // x + y
+    int sqsumdiff = totalsqsum - arrsqsum; // x² + y²
 
-    int product = (sumdiff * sumdiff - sqsumdiff) / 2; //product....
+    int product = (sumdiff * sumdiff - sqsumdiff) / 2; // xy
 
     int discriminant = sqrt(sumdiff * sumdiff - 4 * product);
 
-    int x = (sumdiff + discriminant) / 2; //solve quadratic eq....
+    int x = (sumdiff + discriminant) / 2;
     int y = (sumdiff - discriminant) / 2;
 
     return {x, y};
