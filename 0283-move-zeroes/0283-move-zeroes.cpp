f037bfc80d8a65e3eaa10d9cf01325fc56018e58
Raj Aryan <125886665+rajaryan134@users.noreply.github.com>
2026-03-18 20:03:57 +0530

    Time: 0 ms (100%), Space: 23.8 MB (55.2%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,14 +1,25 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        int j=0;
-        for(int i=0;i<nums.size();i++){
-        if(nums[i]!=0){
-            swap(nums[i],nums[j]);
+        int j = -1;
 
-            j++;
+        // Find first zero index
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] == 0) {
+                j = i;
+                break;
+            }
         }
 
+        // If no zero found, return
+        if (j == -1) return;
+
+        // Move non-zero elements forward
+        for (int i = j + 1; i < nums.size(); i++) {
+            if (nums[i] != 0) {
+                swap(nums[i], nums[j]);
+                j++;
+            }
         }
     }
 };
